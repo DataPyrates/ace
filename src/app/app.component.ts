3c99bdb3f7c14ec05071845ef79ca2dba1651f22
rjcollege <rjcollege123@gmail.com>
2021-05-17 20:27:08 +0530

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataServiceService } from './data-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,42 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private route: Router) {}
+  pages:any;
+  showLevel1 = null;
+  showLevel2 = null;
+  constructor(private route: Router,public dataService: DataServiceService) {
+    this.dataService.getMenus()
+    .subscribe((response)=> {
+        this.pages = response;
+        console.log(this.pages);
+    });
+  }
+
+  toggleLevel1(idx) {
+    if (this.isLevel1Shown(idx)) {
+      this.showLevel1 = null;
+    } else {
+      this.showLevel1 = idx;
+    }
+  };
+
+  isLevel1Shown(idx) {
+    return this.showLevel1 === idx;
+  };
+  
+  isLevel2Shown(idx) {
+    return this.showLevel2 === idx;
+  };
+  
+  toggleLevel2(idx) {
+    if (this.isLevel2Shown(idx)) {
+      this.showLevel1 = null;
+      this.showLevel2 = null;
+    } else {
+      this.showLevel1 = idx;
+      this.showLevel2 = idx;
+    }
+  };
 
   logout(){
     localStorage.clear();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { DataServiceService } from './data-service.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },DataServiceService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
